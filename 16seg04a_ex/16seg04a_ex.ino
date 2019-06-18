048986475afe702bea22fc6e30e286a84848b13d
　 <oberisuku44@gmail.com>
2019-06-18 18:36:17 +0900

    16

---
+++ 16seg04a_ex/16seg04a_ex.ino
@@ -0,0 +1,33 @@
+ void setup() {
+  for(int i = 1; i < 19; i++){
+  pinMode(i, OUTPUT);//pin13は使わない
+  }
+}
+
+const int digits[5][19] = {
+  {1101100000100001001}, // R
+  {1001110111000001001}, // E
+  {1000000011001000011}, // I
+  {0000010000111000000}, // W
+  {0111010000010000000}, // A
+};
+
+void display_number(int num){  
+  for(int w = 0; w <= 19; w++){
+  digitalWrite(w, digits[num][w]);
+  }
+}
+void all_ledoff(){
+  for(int i = 1; i < 19; i++)
+  digitalWrite(i, LOW);
+  }
+
+void loop() {
+  for(int i = 0; i < 5; i++){
+    display_number(i);
+    delay(2000);
+    all_ledoff();
+    delay(50);
+  }
+}
+
